4b383f628619505c814dd3e1fe06dc0592dfcc26
hloth <gitlab@hloth.dev>
2023-06-29 04:43:01 +0400

    add getCheckboxRenderType()

--- README.md
+++ README.md
@@ -12,3 +12,12 @@
 1. Следуйте инструкциям в <https://gitlab.com/x5.io_npmjs_components/getting-started>, чтобы получить доступ к компоненту на npmjs.com
 2. `npm install @x5io/ads_parameter`
 
+## Использование
+
+### `getCheckboxRenderType = (isAdsCampaignActive: boolean | null) => Result`
+
+Если isAdsCampaignActive === null, это означает, что ?ads не представлен
+
+### `resetVerificationStatus = () => void`
+
+Сбросить статус, чтобы `getCheckboxRenderType` не возвращал сохраненный в локальной переменной стейт
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,72 @@
 import Cookies from 'js-cookie'
 
-let status: { wasVerified: false, previousResult: null } | { wasVerified: true, previousResult: boolean } 
-  = { wasVerified: false, previousResult: null }
+let status: { wasVerified: false } | { wasVerified: true, previousResult: Result } 
+  = { wasVerified: false }
 
-export const hasCheckboxes = (companyActive: boolean): boolean => {
-  if(!status.wasVerified) {
-    if (Cookies.get('visited') === undefined) {
-      const params = new URLSearchParams(window.location.search)
-      if (params.has('ads') && companyActive) {
-        Cookies.set('visited', String(Date.now()), { expires: 365 })
+export type Result = 'SHOW_LITE_CHECKBOX'
+  | 'SHOW_FULL_CHECKBOXES'
 
+/**
+ * `SHOW_LITE_CHECKBOX` — means show one lite checkbox\
+ * `SHOW_FULL_CHECKBOXES` — means show all (two) checkboxes\
+ * If [ads] is not present, fallback to `SHOW_FULL_CHECKBOXES`
+ * @param {boolean | null} isAdsCampaignActive If null, indicates that `?ads` is not present
+ */
+
+export const getCheckboxRenderType = (isAdsCampaignActive: boolean | null): Result => {
+  if(status.wasVerified) {
+    return status.previousResult
+  } else {
+    if (Cookies.get('visited') !== undefined) {
+      status = { wasVerified: true, previousResult: 'SHOW_FULL_CHECKBOXES' }
+      return 'SHOW_FULL_CHECKBOXES'
+    } else {
+      if(isAdsCampaignActive === null) {
+        status = { wasVerified: true, previousResult: 'SHOW_FULL_CHECKBOXES' }
+        return 'SHOW_FULL_CHECKBOXES'
+      } else {
         const params = new URLSearchParams(window.location.search)
-        // params.delete('ads')
+        params.delete('ads')
         window.history.pushState({}, '', window.location.pathname + (params.toString() && '?' + params.toString()))
 
-        status = { wasVerified: true, previousResult: false }
-        return false
-      } else {
-        status = { wasVerified: true, previousResult: false }
-        return true
+        if(isAdsCampaignActive === false) {
+          status = { wasVerified: true, previousResult: 'SHOW_FULL_CHECKBOXES' }
+          return 'SHOW_FULL_CHECKBOXES'
+        } else {
+          Cookies.set('visited', String(Date.now()), { expires: 365 })
+          status = { wasVerified: true, previousResult: 'SHOW_LITE_CHECKBOX' }
+          return 'SHOW_LITE_CHECKBOX'
+        }
       }
-    } else {
-      status = { wasVerified: true, previousResult: true }
-      return true
     }
-  } else {
-    return status.previousResult
   }
 }
+
+// export const hasCheckboxes = (companyActive: boolean): boolean => {
+//   if(!status.wasVerified) {
+//     if (Cookies.get('visited') === undefined) {
+//       const params = new URLSearchParams(window.location.search)
+//       if (params.has('ads') && companyActive) {
+//         Cookies.set('visited', String(Date.now()), { expires: 365 })
+
+//         const params = new URLSearchParams(window.location.search)
+//         // params.delete('ads')
+//         window.history.pushState({}, '', window.location.pathname + (params.toString() && '?' + params.toString()))
+
+//         status = { wasVerified: true, previousResult: false }
+//         return false
+//       } else {
+//         status = { wasVerified: true, previousResult: false }
+//         return true
+//       }
+//     } else {
+//       status = { wasVerified: true, previousResult: true }
+//       return true
+//     }
+//   } else {
+//     return status.previousResult
+//   }
+// }
 
 export const resetVerificationStatus = () => {
   status.wasVerified = false 
